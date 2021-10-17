ac6e25c0d70ba4d1694f8d9495619b3fcd861c69
Thiago Maceda <thiagopmaceda@gmail.com>
2021-10-16 21:31:36 -0300

    add absinthe and absinthe plug

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Greenfy.MixProject do
   def application do
     [
       mod: {Greenfy.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :absinthe_plug]
     ]
   end
 
@@ -44,7 +44,9 @@ defmodule Greenfy.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:absinthe, "~> 1.5"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
